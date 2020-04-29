15b8c83a77a57426b856cfe6a64542ba2cd9d30d
Peter O'Rourke <marinedalek@hotmail.com>
2020-04-29 17:44:55 +0100

    Add project files.

---
+++ slitscan/main.cpp
@@ -0,0 +1,19 @@
+#include <raylib.h>
+
+int main()
+{
+	int constexpr screen_width{ 800 };
+	int constexpr screen_height{ 600 };
+	char const * const window_title = "Hello, World!";
+
+	InitWindow(screen_width, screen_height, window_title);
+
+	while (!WindowShouldClose()) {
+		
+		BeginDrawing();
+		{
+			ClearBackground(BLACK);
+		}
+		EndDrawing();
+	}
+}
